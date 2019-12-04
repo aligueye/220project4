83d730d8a2fa1a3c15122fba2d594da1e827cc29
Ali Gueye <aligueye123@gmail.com>
2019-12-03 20:11:42 -0500

    Added method names and main method, no logic doe

---
+++ NameRecord.java
@@ -0,0 +1,42 @@
+/*************************************************************************
+ Corbin Grosso
+ Ali Gueye
+ CSC 220-02
+ 
+ 
+ Description:
+ 
+ Capabilities:
+
+ *************************************************************************/
+
+public class NameRecord {
+
+    private final int START = 1900;
+    private final int DECADES = 11;
+    private String name = "";
+    
+    public NameRecord(String name) {
+        // code code code
+    }
+
+    public String getString() {
+        return name;
+    }
+
+    public int getRank(int decade) {
+        // index of decade 
+        return 1;
+    }
+
+    public int bestYear() {
+        // return year of index with most pop name
+        return 1;
+    }
+
+    public void plot() {
+        // draw shit
+        
+    }
+
+}
---
+++ NameSurfer.java
@@ -0,0 +1,22 @@
+/*************************************************************************
+ Corbin Grosso
+ Ali Gueye
+ CSC 220-02
+ 
+ 
+ Description: Driver Class
+ 
+ Capabilities:
+
+ *************************************************************************/
+
+public class NameSurfer {
+    
+    public static void main(String[] args) {
+        // read all names into array of name records
+
+        // also shit to 'play' 
+        
+    }
+
+}
