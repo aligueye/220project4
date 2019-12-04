32d0e77e125f671bf90595dda191a9e17b26ddef
Corbin Grosso <43792837+RedJayGames@users.noreply.github.com>
2019-12-04 16:51:13 -0500

    Created Constructor

--- NameRecord.java
+++ NameRecord.java
@@ -10,35 +10,64 @@
 
  *************************************************************************/
 
+import java.util.Scanner;
+
 public class NameRecord {
 
-  private final int START = 1900;
-  private final int DECADES = 11;
-  private String name = "";
-  private int[] ranks = new int[DECADES];
-    
+  private final int START = 1900; // the first decade (at index 0)
+  private final int DECADES = 11; // the number of decades in the dataset
+  private String name = ""; // the baby name
+  private int[] ranks = new int[DECADES]; // the rank of the name in each decade (a rank of 0 means it was not popular enough to recieve a rank)
   
-  public NameRecord(String name) {
-    // code code code
-    this.name = name;
+  public NameRecord(String line) {
+    
+    Scanner scnr = new Scanner(line);
+    name = scnr.next();
+    
+    for (int i = 0; i < DECADES; i++) {
+      
+      ranks[i] = Integer.parseInt(scnr.next());
+      
+    }
+    
   }
 
-  public String getString() {
+  public String getName() {
+
     return name;
+
   }
 
   public int getRank(int decade) {
-    // index of decade 
-    return 1;
+
+    return ranks[decade];
+
   }
 
   public int bestYear() {
+
     // return year of index with most pop name
     return 1;
+
   }
 
   public void plot() {
+
     // draw shit
+      
+  }
+  
+  public String toString() {
+    
+    String output = "Name: " + name;
+    for (int i = 0; i < DECADES; i++) {
+      
+      output += "\nRank in " + (START + (i * 10)) + ": " + ranks[i];
+      
+    }
+    
+    return output;
+    
   }
 
 }
