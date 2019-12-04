df6273930cb840b1d1f739b0665b9551047b4c73
Ali Gueye <aligueye123@gmail.com>
2019-12-04 16:02:41 -0500

    can read all lines of file

--- NameSurfer.java
+++ NameSurfer.java
@@ -9,12 +9,30 @@
  Capabilities:
 
  *************************************************************************/
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class NameSurfer {
     
     public static void main(String[] args) {
         // read all names into array of name records
-        // while shit has nextline next = name, nextInt into array of 12
+        File file = new File("name_data.txt");
+        int count = 0;
+        try {
+            Scanner scnr = new Scanner(file);
+            
+            while (scnr.hasNextLine()) {
+                scnr.nextLine();
+                count++;
+            }
+            
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(count);
         // also shit to 'play' 
         
     }
